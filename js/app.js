dc2fdac0695bf50938925c6aa5391691b20650c2
lisacowan27 <cowan_lisa@att.net>
2017-12-15 10:48:56 -0600

    feat: set up markers

--- js/app.js
+++ js/app.js
@@ -1,45 +1,187 @@
 /* Knockout here -- used to handle the list, filter and any other information that is subject to changing state -- the Model stuff */
 
-var locations = [
-    {
-    title: 'Hippie Hollow Park',
-    location: {
-        lat: 30.413155,
-        lng: -97.884064}
-    },
-    {
-    title: 'Barton Springs',
-    location: {
-        lat: 30.2640024,
-        lng: -97.7709723}
-    },
-    {title: 'Woodlawn (Pease Mansion)',
-    location: {
-        lat: 30.2640901,
-        lng: -97.797237}
-    },
-    {
-    title: 'West Sixth Street Bridge',
-    location: {
-        lat: 30.2730245,
-        lng: -97.7601438}
-    },
-    {
-    title: 'Austin State Hospital',
-    location: {
-        lat: 30.3075604,
-        lng: -97.7392435}
-    }
+var locations = [{title: 'Hippie Hollow Park', location: {lat: 30.413155, lng: -97.884064}},
+    {title: 'Barton Springs', location: {lat: 30.2640024, lng: -97.7709723}},
+    {title: 'Woodlawn (Pease Mansion)', location: {lat: 30.2640901, lng: -97.797237}},
+    {title: 'West Sixth Street Bridge', location: {lat: 30.2730245, lng: -97.7601438}},
+    {title: 'Austin State Hospital', location: {lat: 30.3075604, lng: -97.7392435}}
 ];
 
 
+/* Maps API here -- used for creating markers, tracking click events on markers, making the map and refreshing the map. -- API stuff */
+
+var map;
+
+//Create empty variable markers for places of interest
+var markers = [];
+
+function initMap() {
+    // Create a styles array to use with the map.
+    var styles = [
+      {
+        featureType: 'water',
+        stylers: [
+          { color: '#19a0d8' }
+        ]
+      },{
+        featureType: 'administrative',
+        elementType: 'labels.text.stroke',
+        stylers: [
+          { color: '#ffffff' },
+          { weight: 6 }
+        ]
+      },{
+        featureType: 'administrative',
+        elementType: 'labels.text.fill',
+        stylers: [
+          { color: '#e85113' }
+        ]
+      },{
+        featureType: 'road.highway',
+        elementType: 'geometry.stroke',
+        stylers: [
+          { color: '#efe9e4' },
+          { lightness: -40 }
+        ]
+      },{
+        featureType: 'transit.station',
+        stylers: [
+          { weight: 9 },
+          { hue: '#e85113' }
+        ]
+      },{
+        featureType: 'road.highway',
+        elementType: 'labels.icon',
+        stylers: [
+          { visibility: 'off' }
+        ]
+      },{
+        featureType: 'water',
+        elementType: 'labels.text.stroke',
+        stylers: [
+          { lightness: 100 }
+        ]
+      },{
+        featureType: 'water',
+        elementType: 'labels.text.fill',
+        stylers: [
+          { lightness: -100 }
+        ]
+      },{
+        featureType: 'poi',
+        elementType: 'geometry',
+        stylers: [
+          { visibility: 'on' },
+          { color: '#f0e4d3' }
+        ]
+      },{
+        featureType: 'road.highway',
+        elementType: 'geometry.fill',
+        stylers: [
+          { color: '#efe9e4' },
+          { lightness: -25 }
+        ]
+      }
+    ];
+
+    // Constructor creates a new map - only center and zoom are required.
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 30.267153, lng: -97.7430608},
+      zoom: 13,
+      styles: styles,
+      mapTypeControl: false
+    });
+
+    var largeInfowindow = new google.maps.InfoWindow();
+    var bounds = new google.maps.LatLngBounds();
+
+    // Style the markers a bit. This will be our listing marker icon.
+    //var defaultIcon = makeMarkerIcon('0091ff');
+
+    // Create a "highlighted location" marker color for when the user
+    // mouses over the marker.
+    //var highlightedIcon = makeMarkerIcon('FFFF24');
+
+    // This function takes in a COLOR, and then creates a new marker
+      // icon of that color. The icon will be 21 px wide by 34 high, have an origin
+      // of 0, 0 and be anchored at 10, 34).
+      /*function makeMarkerIcon(markerColor) {
+        var markerImage = new google.maps.MarkerImage(
+          'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
+          '|40|_|%E2%80%A2',
+          new google.maps.Size(21, 34),
+          new google.maps.Point(0, 0),
+          new google.maps.Point(10, 34),
+          new google.maps.Size(21,34));
+        return markerImage;
+      }*/
+
+    for (var i = 0; i < locations.length; i++) {
+        // Get the position from the location array.
+        var position = locations[i].location;
+        var title = locations[i].title;
+        // Create a marker per location, and put into markers array.
+        var marker = new google.maps.Marker({
+            map: map,
+            position: position,
+            title: title,
+            animation: google.maps.Animation.DROP,
+            //icon: defaultIcon,
+            id: i
+        });
+        // Push the marker to our array of markers.
+        markers.push(marker);
+        console.log('here are markers ' + markers);
+          // Create an onclick event to open the large infowindow at each marker.
+          marker.addListener('click', function() {
+            populateInfoWindow(this, largeInfowindow);
+          });
+          bounds.extend(markers[i].position);
+          // Two event listeners - one for mouseover, one for mouseout,
+          // to change the colors back and forth.
+          /*marker.addListener('mouseover', function() {
+            this.setIcon(highlightedIcon);
+          });
+          marker.addListener('mouseout', function() {
+            this.setIcon(defaultIcon);
+          });*/
+          map.fitBounds(bounds);
+    }
+}
+
+    function populateInfoWindow(marker, infowindow) {
+        // Check to make sure the infowindow is not already opened on this marker.
+        if (infowindow.marker != marker) {
+          infowindow.marker = marker;
+          infowindow.setContent('<div>' + marker.title + '</div>');
+          infowindow.open(map, marker);
+          // Make sure the marker property is cleared if the infowindow is closed.
+          infowindow.addListener('closeclick',function(){
+            infowindow.setMarker = null;
+          });
+        }
+      }
 
 
-/* Maps API here -- used for creating markers, tracking click events on markers, making the map and refreshing the map. -- API stuff
+ /*   var marker = new google.maps.Marker({
+          position: home,
+          map: map,
+          styles: styles,
+          title: 'This is home!'
+        });
+        var infowindow = new google.maps.InfoWindow({
+          content: 'This is a happy house'
+        });
+        marker.addListener('click', function(){
+          infowindow.open(map, marker);
+        });*/
 
-Tracking click event on *list items* should be handled with KO -- View stuff */
+
+
+
+/* Tracking click event on *list items* should be handled with KO -- View stuff */
 
 
 
 
-/* Creating markers as part of your VM is recommended, but the cannot be knockout observables.
+/* Creating markers as part of your VM is recommended, but the cannot be knockout observables.*/
