5ba3b834f9aaf58e3d4acc1a6576ecda6608ef6b
lisacowan27 <cowan_lisa@att.net>
2017-12-19 14:31:05 -0600

    feat: set up files for ko.utils

--- js/app.js
+++ js/app.js
@@ -246,11 +246,11 @@ function initMap() {
 
           map.fitBounds(bounds);
     }
-}
+
 
         // Listen for the event fired when the user selects a prediction and clicks
         // "go" more details for that place.
-        document.getElementById('go-places').addEventListener('click', textSearchPlaces);
+        //document.getElementById('go-places').addEventListener('click', textSearchPlaces);
 
         // This function will loop through the listings and hide them all.
       function hideMarkers(markers) {
@@ -272,123 +272,7 @@ function initMap() {
         }
       }
 
-
-      // This function fires when the user selects a searchbox picklist item.
-      // It will do a nearby search using the selected query string or place.
-      function searchBoxPlaces(searchBox) {
-        hideMarkers(placeMarkers);
-        var places = searchBox.getPlaces();
-        if (places.length == 0) {
-          window.alert('We did not find any places matching that search!');
-        } else {
-        // For each place, get the icon, name and location.
-          createMarkersForPlaces(places);
-        }
-      }
-
-      // This function firest when the user select "go" on the places search.
-      // It will do a nearby search using the entered query string or place.
-      function textSearchPlaces() {
-        var bounds = map.getBounds();
-        hideMarkers(placeMarkers);
-        var placesService = new google.maps.places.PlacesService(map);
-        placesService.textSearch({
-          query: document.getElementById('places-search').value,
-          bounds: bounds
-        }, function(results, status) {
-          if (status === google.maps.places.PlacesServiceStatus.OK) {
-            createMarkersForPlaces(results);
-          }
-        });
-      }
-
-      // This function creates markers for each place found in either places search.
-      function createMarkersForPlaces(places) {
-        var bounds = new google.maps.LatLngBounds();
-        for (var i = 0; i < places.length; i++) {
-          var place = places[i];
-          var icon = {
-            url: place.icon,
-            size: new google.maps.Size(35, 35),
-            origin: new google.maps.Point(0, 0),
-            anchor: new google.maps.Point(15, 34),
-            scaledSize: new google.maps.Size(25, 25)
-          };
-          // Create a marker for each place.
-          var marker = new google.maps.Marker({
-            map: map,
-            icon: icon,
-            title: place.name,
-            position: place.geometry.location,
-            id: place.place_id
-          });
-          // Create a single infowindow to be used with the place details information
-          // so that only one is open at once.
-          var placeInfoWindow = new google.maps.InfoWindow();
-          // If a marker is clicked, do a place details search on it in the next function.
-          marker.addListener('click', function() {
-            if (placeInfoWindow.marker == this) {
-              console.log("This infowindow already is on this marker!");
-            } else {
-              getPlacesDetails(this, placeInfoWindow);
-            }
-          });
-          placeMarkers.push(marker);
-          if (place.geometry.viewport) {
-            // Only geocodes have viewport.
-            bounds.union(place.geometry.viewport);
-          } else {
-            bounds.extend(place.geometry.location);
-          }
-        }
-        map.fitBounds(bounds);
-      }
-
-    // This is the PLACE DETAILS search - it's the most detailed so it's only
-    // executed when a marker is selected, indicating the user wants more
-    // details about that place.
-    function getPlacesDetails(marker, infowindow) {
-      var service = new google.maps.places.PlacesService(map);
-      service.getDetails({
-        placeId: marker.id
-      }, function(place, status) {
-        if (status === google.maps.places.PlacesServiceStatus.OK) {
-          // Set the marker property on this infowindow so it isn't created again.
-          infowindow.marker = marker;
-          var innerHTML = '<div>';
-          if (place.name) {
-            innerHTML += '<strong>' + place.name + '</strong>';
-          }
-          if (place.formatted_address) {
-            innerHTML += '<br>' + place.formatted_address;
-          }
-          if (place.formatted_phone_number) {
-            innerHTML += '<br>' + place.formatted_phone_number;
-          }
-          if (place.opening_hours) {
-            innerHTML += '<br><br><strong>Hours:</strong><br>' +
-                place.opening_hours.weekday_text[0] + '<br>' +
-                place.opening_hours.weekday_text[1] + '<br>' +
-                place.opening_hours.weekday_text[2] + '<br>' +
-                place.opening_hours.weekday_text[3] + '<br>' +
-                place.opening_hours.weekday_text[4] + '<br>' +
-                place.opening_hours.weekday_text[5] + '<br>' +
-                place.opening_hours.weekday_text[6];
-          }
-          if (place.photos) {
-            innerHTML += '<br><br><img src="' + place.photos[0].getUrl(
-                {maxHeight: 100, maxWidth: 200}) + '">';
-          }
-          innerHTML += '</div>';
-          infowindow.setContent(innerHTML);
-          infowindow.open(map, marker);
-          // Make sure the marker property is cleared if the infowindow is closed.
-          infowindow.addListener('closeclick', function() {
-            infowindow.marker = null;
-          });
-        }
-      });
-    }
+}
 
 
 
@@ -399,7 +283,7 @@ function initMap() {
 
 /* Creating markers as part of your VM is recommended, but the cannot be knockout observables.*/
 
-var ViewModel = function() {
+var ViewPlaces = function() {
     // self will always map to the VM
     var self = this;
     var applyFilter;
@@ -423,4 +307,4 @@ var ViewModel = function() {
 };
 
 // Apply
-ko.applyBindings(new ViewModel());
+ko.applyBindings(new ViewPlaces());
