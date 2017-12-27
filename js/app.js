cf3bb3b8fe776605ae1914e9fc1770d42127996f
lisacowan27 <cowan_lisa@att.net>
2017-12-27 15:16:59 -0600

    feat: add photo credits to html; add styles

--- js/app.js
+++ js/app.js
@@ -177,9 +177,6 @@ var ViewPlaces = function() {
             //timeout is cleared if wikipedia link is loaded successfully
             clearTimeout(wikiTimeout);
         });
-
-
-
     }
 
 
@@ -241,11 +238,163 @@ var ViewPlaces = function() {
 
 function initMap() {
     // Create a styles array to use with the map.
+    var styles = [
+      {
+          featureType: "administrative",
+          elementType: "labels.text.fill",
+          stylers: [
+              {
+                  color: "#6195a0"
+              }
+          ]
+      },
+      {
+          featureType: "landscape",
+          elementType: "all",
+          stylers: [
+              {
+                  color: "#f2f2f2"
+              }
+          ]
+      },
+      {
+          featureType: "landscape",
+          elementType: "geometry.fill",
+          stylers: [
+              {
+                  color: "#ffffff"
+              }
+          ]
+      },
+      {
+          featureType: "poi",
+          elementType: "all",
+          stylers: [
+              {
+                  visibility: "off"
+              }
+          ]
+      },
+      {
+          featureType: "poi.park",
+          elementType: "geometry.fill",
+          stylers: [
+              {
+                  color: "#e6f3d6"
+              },
+              {
+                  visibility: "on"
+              }
+          ]
+      },
+      {
+          featureType: "road",
+          elementType: "all",
+          stylers: [
+              {
+                  saturation: -100
+              },
+              {
+                  lightness: 45
+              },
+              {
+                  visibility: "simplified"
+              }
+          ]
+      },
+      {
+          featureType: "road.highway",
+          elementType: "all",
+          stylers: [
+              {
+                  visibility: "simplified"
+              }
+          ]
+      },
+      {
+          featureType: "road.highway",
+          elementType: "geometry.fill",
+          stylers: [
+              {
+                  color: "#f4d2c5"
+              },
+              {
+                  visibility: "simplified"
+              }
+          ]
+      },
+      {
+          featureType: "road.highway",
+          elementType: "labels.text",
+          stylers: [
+              {
+                  color: "#4e4e4e"
+              }
+          ]
+      },
+      {
+          featureType: "road.arterial",
+          elementType: "geometry.fill",
+          stylers: [
+              {
+                  color: "#f4f4f4"
+              }
+          ]
+      },
+      {
+          featureType: "road.arterial",
+          elementType: "labels.text.fill",
+          stylers: [
+              {
+                  color: "#787878"
+              }
+          ]
+      },
+      {
+          featureType: "road.arterial",
+          elementType: "labels.icon",
+          stylers: [
+              {
+                  visibility: "off"
+              }
+          ]
+      },
+      {
+          featureType: "transit",
+          elementType: "all",
+          stylers: [
+              {
+                  visibility: "off"
+              }
+          ]
+      },
+      {
+          featureType: "water",
+          elementType: "all",
+          stylers: [
+              {
+                  color: "#eaf6f8"
+              },
+              {
+                  visibility: "on"
+              }
+          ]
+      },
+      {
+          featureType: "water",
+          elementType: "geometry.fill",
+          stylers: [
+              {
+                  color: "#eaf6f8"
+              }
+          ]
+      }
+  ];
 
     // Constructor creates a new map - only center and zoom are required.
     map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: 46.188294, lng: -86.4655739},
-      //styles: styles,
+      styles: styles,
       zoom: 10
     });
 
